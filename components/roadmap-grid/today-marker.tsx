9c23e668ccc2e54bd4d7cd9bb440f4079dd34c3c
Dan McQuillan <djmcquillan76@gmail.com>
2023-01-10 15:13:08 -0800

    feat: 248 today toggle cleanup from PR

--- components/roadmap-grid/today-marker.tsx
+++ components/roadmap-grid/today-marker.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { dayjs } from '../../lib/client/dayjs';
-import { Center, Text } from '@chakra-ui/react';
 
 import { globalTimeScaler } from '../../lib/client/TimeScaler';
 import styles from './today-marker.module.css';
@@ -8,7 +7,6 @@ import styles from './today-marker.module.css';
 export function TodayMarker({ ticksLength }: {ticksLength:number}) {
   const today = dayjs();
   const [percentLeft, setPercentLeft] = useState(0);
-  // const [isLineVisible, setIsLineVisible] = useState(true);
 
   useEffect(() => {
     const percentage = Number((globalTimeScaler.getPercentile(today.toDate()) * 100))
@@ -22,9 +20,6 @@ export function TodayMarker({ ticksLength }: {ticksLength:number}) {
     }}>
       <div className={styles.todayMarkerPointer} />
       <div className={styles.todayMarker} />
-      {/* <Center cursor="pointer" onClick={() => setIsLineVisible(!isLineVisible)}>
-        <Text className={styles.todayMarkerText} fontSize={{ sm:"16px", md:"19px", lg:"19px" }}>TODAY</Text>
-      </Center> */}
     </div>
   );
 }
